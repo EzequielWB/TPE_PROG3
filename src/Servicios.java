ca641c05c03656dc0900704dbbdec4cae9c8cf7a
micadiazz1 <micaelaexactas1@gmail.com>
2026-06-17 15:15:07 -0300

    agregue solucion back

--- src/Servicios.java
+++ src/Servicios.java
@@ -10,9 +10,10 @@ public class Servicios {
 
     // Lista con todos los paquetes para servicio 2 y 3, Complejidad O(n) (recorre toda la lista)
     private List<Paquete> todosLosPaquetes;
+    private Solucion mejorSolucionBacktracking;
+    private int estadosGenerados; //metrica back
 
      //Complejidad O(N + M), N archivo camiones.csv, M archivo paquetes.csv. Se recorre cada .csv una vez
-
     public Servicios(String pathCamiones, String pathPaquetes) {
         this.camiones = new ArrayList<>();
         this.indiceCodigoPaquete = new HashMap<>();
@@ -114,6 +115,91 @@ public class Servicios {
         }
         return resultado;
     }
+    /*
+        Estrategia Backtraking
+        Explorar exhaustivamente todas las combinaciones posibles de asignación
+        de paquetes a camiones. Para cada paquete, intentar asignarlo a cada
+        camión que cumpla las restricciones (capacidad y refrigeración).
+        Se mantiene la mejor solución encontrada (mínimo peso sin asignar).
+     */
+        public Solucion backtracking() {
+            this.mejorSolucionBacktracking = null;
+            this.estadosGenerados = 0;
+            int[]pesoActualCamion= new int[this.camiones.size()];
+            List<Paquete>[]caminoActualCamiones = new ArrayList[camiones.size()];
+            for (int i = 0; i < this.camiones.size(); i++) {
+                caminoActualCamiones[i] = new ArrayList<>();
+            }
+            back(0,pesoActualCamion,caminoActualCamiones);
+            return this.mejorSolucionBacktracking;
+        }
+        public void back(int indicePaquete,int[] pesoActualCamion, List<Paquete>[] caminoActualCamiones) {
+            //metrica back
+            estadosGenerados++;
+
+            //caso base si ya se recorrio todos los paquetes que corte
+            if(indicePaquete== todosLosPaquetes.size()){
+                Solucion candidato=ConstruirSolucion(caminoActualCamiones);
+                //es mejor que la que se encontro?
+                if(mejorSolucionBacktracking==null || candidato.getPesoNoAsignado()<mejorSolucionBacktracking.getPesoNoAsignado()){
+                    mejorSolucionBacktracking=candidato;
+                }
+                return;
+            }
+            Paquete paqueteActual = todosLosPaquetes.get(indicePaquete);
+            int pesoPaquete = paqueteActual.getPesoKg();
+            boolean tieneAlimentos=paqueteActual.getContieneAlimentos();
+
+            for(int i=0;i<camiones.size();i++){
+                Camion camion = camiones.get(i);
+                if(puedoAsignar(camion,pesoPaquete,tieneAlimentos,pesoActualCamion[i])){
+                    //le sumo el peso del paquete y agrego al camino actual (camion) el paquete actual
+                    pesoActualCamion[i]+=pesoPaquete;
+                    caminoActualCamiones[i].add(paqueteActual);
+                    //back
+                    back(indicePaquete+1,pesoActualCamion,caminoActualCamiones);
+                    //deshago lo que hice antes de volver a probar con otro camion
+                    pesoActualCamion[i]-=pesoPaquete;
+                    caminoActualCamiones[i].remove(caminoActualCamiones[i].size()-1);
+                }
+            }
+            //no asigno el paquete a ningun camion
+            back(indicePaquete + 1, pesoActualCamion, caminoActualCamiones);
+        }
+
+    public int getEstadosGenerados() {
+        return estadosGenerados;
+    }
+
+    // Verificar si puedo asignar un paquete a un camión
+    private boolean puedoAsignar(Camion camion, int peso, boolean tieneAlimentos, int pesoActual) {
+        //No superar capacidad
+        if (pesoActual + peso > camion.getCapacidadKg()) {
+            return false;
+        }
+        // Alimentos solo en refrigerados
+        if (tieneAlimentos && !camion.getEstaRefrigerado()) {
+            return false;
+        }
+        return true;
+    }
+        public Solucion ConstruirSolucion(List<Paquete>[] caminoActualCamiones){
+            Solucion solucion = new Solucion();
+            double pesoNoAsignado = 0;
+            for (int i = 0; i < camiones.size(); i++) {
+                Camion c = camiones.get(i);
+                List<Paquete> paquetesAsignados = caminoActualCamiones[i];
+                for (Paquete p : paquetesAsignados) {
+                    solucion.agregarAsignacion(c, p);
+                }
+                double pesoAsignado = paquetesAsignados.stream().mapToDouble(Paquete::getPesoKg).sum();
+                if (pesoAsignado > c.getCapacidadKg()) {
+                    pesoNoAsignado += pesoAsignado - c.getCapacidadKg();
+                }
+            }
+            solucion.setPesoNoAsignado(pesoNoAsignado);
+            return solucion;
+        }
 
     /*
      * ESTRATEGIA GREEDY:
